80f7b8190b54ee2d8171f633867d5ba5fc51ba85
GFardad <greengame986@gmail.com>
2025-11-29 12:27:25 +0330

    Moz

--- E1/P17/Main.py
+++ E1/P17/Main.py
@@ -1,14 +1,22 @@
 a_list = ["A", "B", "C", "D"]
 b_list = ["B", "C"]
 result = []
-
-# print((len(a_list)))
+i = 0
 
 for a_item in a_list:
     found_in_b = False
-        for b_item in b_list:
-            if a_item == b_item:
-                result.append(a_item)
-        else:
-            i += 1
+    for b_item in b_list:
+        if a_item == b_item:
+            found_in_b = True
+            break
+    if not found_in_b:
+        result.append(a_item)
+
+
 print(result)
+
+
+a_list = ["A", "B", "C", "D"]
+b_list = ["B", "C"]
+result = (set(a_list)) - (set(b_list))
+print(sorted(result))
