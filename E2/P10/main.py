81a9de98b41b158366ba3fb2818ff6e90ae6350f
GFardad <greengame986@gmail.com>
2025-12-20 14:02:15 +0330

    Moz

---
+++ E2/P10/main.py
@@ -0,0 +1,75 @@
+import re
+from tracemalloc import start, stop
+from unittest import result
+
+
+visited = set()
+index = 0
+
+
+def C2I(inp_string):
+    if type(inp_string) != str:
+        return inp_string
+    else:
+        char = inp_string[0]
+        num = inp_string[1]
+        char = (ord(char.lower())) - (ord("a"))
+        return (int(char), int(num) - 1)
+
+
+def calc_offset(x, y):
+    offset = [
+        [x + 2, y + 1],
+        [x + 2, y - 1],
+        [x - 2, y + 1],
+        [x - 2, y - 1],
+        [x + 1, y + 2],
+        [x + 1, y - 2],
+        [x - 1, y + 2],
+        [x - 1, y - 2],
+    ]
+    offset_t = [tuple(item) for item in offset]
+    return tuple(offset_t)
+
+
+def validator(new_points):
+    global visited
+    global index
+    clean_new_point = []
+    for item in new_points:
+        if item[0] <= 7 and item[1] <= 7 and item[0] >= 0 and item[1] >= 0:
+            clean_new_point.append(item)
+    clean_new_point = set(clean_new_point) - visited
+    visited.update(clean_new_point)
+    index += 1
+    return clean_new_point
+
+
+def main(local_start_point, index):
+
+    new_point = calc_offset(*local_start_point)
+
+
+def solve(start_cord, stop_cord):
+    start_cord = C2I(start_cord)
+    stop_cord = C2I(stop_cord)
+    queue = [((start_cord), 0)]
+
+    while queue:
+        cordinate, dist = queue.pop(0)
+        if cordinate == stop_cord:
+            return dist
+        next_moves = validator(calc_offset(*cordinate))
+        for next_move in next_moves:
+            queue.append((next_move, dist + 1))
+
+
+start = input("Give me a number for starting point of BSF : ")
+stop = input("Give me a number for targrt point of BSF : ")
+if C2I(start) == C2I(stop):
+    print(
+        f"strat and stop point are same! both of them is {stop.upper()} \nu wana go whare?"
+    )
+else:
+    result = solve(start, stop)
+    print(f"shortest path bitwhin {start.upper()} and {stop.upper()} is {result}")
--- E2/P11/main.py
+++ E2/P11/main.py
@@ -1,70 +1,37 @@
-import re
-from tracemalloc import start, stop
-from unittest import result
+from E2.P11.main import spliter
+
+game_status = "X 7/ 90 X X 8/ 23 6/ X 8/1"
 
 
-visited = set()
-index = 0
+def spliter():
+    status_list = game_status.split(" ")
+
+    return status_list
 
 
-def C2I(inp_string):
-    if type(inp_string) != str:
-        return inp_string
-    else:
-        char = inp_string[0]
-        num = inp_string[1]
-        char = (ord(char.lower())) - (ord("a"))
-        return (int(char), int(num) - 1)
+def solve(status_list):
+    result = 0
+    for curr, i in zip(status_list, range(len(status_list))):
+        # next_frame_sper = sts(status_list[i + 1][0])
+        next_frame_full = status_list[i + 1]
+        if "X" in curr:
+            if "/" in (status_list[i + 1]):
+                result = result + sts(curr, next_frame_full)
+            else:
+                result = result + sts(curr, next_frame_full)
+        # elif "/" in curr:
+        # if "X" in status_list[i + 1]:
+
+        # else:
+        #     result = result + 10 + int(status_list[i + 1][0])
 
 
-def calc_offset(x, y):
-    offset = [
-        [x + 2, y + 1],
-        [x + 2, y - 1],
-        [x - 2, y + 1],
-        [x - 2, y - 1],
-        [x + 1, y + 2],
-        [x + 1, y - 2],
-        [x - 1, y + 2],
-        [x - 1, y - 2],
-    ]
-    offset_t = [tuple(item) for item in offset]
-    return tuple(offset_t)
+def sts(inp, next_frame):
 
-
-def validator(new_points):
-    global visited
-    global index
-    clean_new_point = []
-    for item in new_points:
-        if item[0] <= 7 and item[1] <= 7 and item[0] >= 0 and item[1] >= 0:
-            clean_new_point.append(item)
-    clean_new_point = set(clean_new_point) - visited
-    visited.update(clean_new_point)
-    index += 1
-    return clean_new_point
-
-
-def main(local_start_point, index):
-
-    new_point = calc_offset(*local_start_point)
-
-
-def solve(start_cord, stop_cord):
-    start_cord = C2I(start_cord)
-    stop_cord = C2I(stop_cord)
-    queue = [((start_cord), 0)]
-
-    while queue:
-        cordinate, dist = queue.pop(0)
-        if cordinate == stop_cord:
-            return dist
-        next_moves = validator(calc_offset(*cordinate))
-        for next_move in next_moves:
-            queue.append((next_move, dist + 1))
-
-
-start = input("Give me a number for starting point of BSF : ")
-stop = input("Give me a number for targrt point of BSF : ")
-result = solve(start, stop)
-print(f"shortest path bitwhin {start.upper()} and {stop.upper()} is {result}")
+    if len(inp) == 2:
+        sepprated_number = [str(inp)[i] for i in range(len(str(inp)))]
+    if "X" in inp:
+        return 10
+    elif "/" in inp:
+    
+    return int()
